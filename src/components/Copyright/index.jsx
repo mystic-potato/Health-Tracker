1db18ceb16317638e59a1281a8f89561d4f30ed6
mystic-potato <adititkumar@gmail.com>
2021-11-27 19:05:05 +0530

    Update index.jsx

--- src/components/Copyright/index.jsx
+++ src/components/Copyright/index.jsx
@@ -4,7 +4,7 @@ import Typography from '@material-ui/core/Typography';
 function Copyright() {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
-        {'Created for TARP  '}
+        {'... '}
         {new Date().getFullYear()}
         {'.'}
         </Typography>
